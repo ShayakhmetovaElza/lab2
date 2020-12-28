9ac1be6f1e2affe3f652065e773d5fdde664082a
ShayakhmetovaElza <63636579+ShayakhmetovaElza@users.noreply.github.com>
2020-12-28 13:09:10 +0300

    project

---
+++ ClientSharp/ClientSharp/ClientSharp/Message.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientSharp
+{
+	public enum Messages
+	{
+		M_INIT,
+		M_EXIT,
+		M_GETDATA,
+		M_NODATA,
+		M_TEXT,
+		M_CONFIRM
+	};
+
+	public enum Members
+	{
+		M_BROKER = 0,
+		M_ALL = 10,
+		M_USER = 100
+	};
+
+	public struct MsgHeader
+	{
+		public int m_To;
+		public int m_From;
+		public Messages m_Type;
+		public int m_Size;
+	};
+	class Message
+	{
+		private MsgHeader M_header;
+		private string M_data;
+
+		public Message()
+		{
+			M_header.m_To = 0;
+			M_header.m_From = 0;
+			M_header.m_Type = Messages.M_NODATA;
+			M_header.m_Size = 0;
+		}
+		public Message(int to, int from, Messages type = Messages.M_TEXT, string data = "")
+		{
+			M_header.m_To = to;
+			M_header.m_From = from;
+			M_header.m_Type = type;
+			M_header.m_Size = data.Length;
+			M_data = data;
+		}
+
+		public string getMsgText()
+        {
+			return M_data;
+        }
+
+		void Send(Socket s)
+		{
+			s.Send(BitConverter.GetBytes(M_header.m_To), sizeof(int), SocketFlags.None);
+			s.Send(BitConverter.GetBytes(M_header.m_From), sizeof(int), SocketFlags.None);
+			s.Send(BitConverter.GetBytes((int)M_header.m_Type), sizeof(int), SocketFlags.None);
+			s.Send(BitConverter.GetBytes(M_data.Length), sizeof(int), SocketFlags.None);
+			if (M_header.m_Size != 0)
+			{
+				s.Send(Encoding.UTF8.GetBytes(M_data), M_data.Length, SocketFlags.None);
+			}
+		}
+
+		public MsgHeader Receive(Socket s)
+		{
+			byte[] my_byte = new byte[4];
+			s.Receive(my_byte, sizeof(int), SocketFlags.None);
+			M_header.m_To = BitConverter.ToInt32(my_byte, 0);
+
+			my_byte = new byte[4];
+			s.Receive(my_byte, sizeof(int), SocketFlags.None);
+			M_header.m_From = BitConverter.ToInt32(my_byte, 0);
+
+			my_byte = new byte[4];
+			s.Receive(my_byte, sizeof(int), SocketFlags.None);
+			M_header.m_Type = (Messages)BitConverter.ToInt32(my_byte, 0);
+
+			my_byte = new byte[4];
+			s.Receive(my_byte, sizeof(int), SocketFlags.None);
+			M_header.m_Size = BitConverter.ToInt32(my_byte, 0);
+
+			if (M_header.m_Size != 0)
+			{
+				my_byte = new byte[M_header.m_Size];
+				s.Receive(my_byte, M_header.m_Size, SocketFlags.None);
+				M_data = Encoding.UTF8.GetString(my_byte, 0, M_header.m_Size);
+			}
+			return M_header;
+		}
+
+		public void SendMessage(Socket s, int To, int From, Messages Type = Messages.M_TEXT, string Data = "")
+		{
+			Message msg = new Message(To, From, Type, Data);
+			msg.Send(s);
+		}
+	}
+}
+
---
+++ ClientSharp/ClientSharp/ClientSharp/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ClientSharp
+{
+	class Program
+    {
+        static int myId;
+        static Mutex hMutex = new Mutex();
+        static int nPort = 12345;
+        static IPAddress ipAddr = IPAddress.Parse("127.0.0.1");
+
+        static void MyChannel(IPEndPoint endPoint, ref Message msg, ref MsgHeader h_msg, ref Socket client, Messages Type, int ID_ToClient, int ID_FromClient, string textMsg = "")
+        {
+            try
+            {
+                client.Connect(endPoint);
+            }
+            catch (SocketException ex)
+            {
+                Console.Write("Ошибка подключения!");
+                Environment.Exit(0);
+            }
+            finally
+            {
+                msg.SendMessage(client, ID_ToClient, ID_FromClient, Type, textMsg);
+                h_msg = msg.Receive(client);
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+            }
+
+        }
+
+        static void GetDataFromServer()
+        {
+            while (true)
+            {
+                Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPEndPoint endPoint = new IPEndPoint(ipAddr, nPort);
+
+                Message msg = new Message();
+                MsgHeader h_msg = new MsgHeader();
+
+                MyChannel(endPoint, ref msg, ref h_msg, ref client, Messages.M_GETDATA, 0, myId);
+
+                string MsgText = "";
+
+                if (h_msg.m_Type == Messages.M_TEXT)
+                {
+                    MsgText = msg.getMsgText();
+                    hMutex.WaitOne();
+                    Console.WriteLine("Сообщение от клиента: " + MsgText);
+                    hMutex.ReleaseMutex();
+                }
+                Thread.Sleep(2000);
+            }
+        }
+
+        static void ConnectToServer(IPEndPoint endPoint, ref Message m, ref MsgHeader h_msg, ref Socket client)
+        {
+
+            MyChannel(endPoint, ref m, ref h_msg, ref client, Messages.M_INIT, 0, 0);
+
+            if (h_msg.m_Type == Messages.M_CONFIRM)
+            {
+                myId = h_msg.m_To;
+                Console.WriteLine("Ваш ID = " + myId);
+                Thread getDataThread = new Thread(GetDataFromServer);
+                getDataThread.Start();
+            }
+            else
+            {
+                Console.WriteLine("Ошибка. Клиент не подключен." );
+                return;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите: 1 - подключиться к серверу. \n 0 - выйти.\n");
+            int result = Convert.ToInt32(Console.ReadLine());
+            if(result == 0) Environment.Exit(0);
+            if(result == 1)
+            {
+                //Подключение к серверу
+                Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPEndPoint endPoint = new IPEndPoint(ipAddr,nPort);
+          
+                Message msg = new Message();
+                MsgHeader h_msg = new MsgHeader();
+                ConnectToServer(endPoint, ref msg, ref h_msg, ref client);
+
+                while (true)
+                {
+                    client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    endPoint = new IPEndPoint(ipAddr, nPort);
+
+                    string textMsg;
+                    int ID_ToClient;
+
+                    Console.WriteLine("Отправить сообщение: 0 - всем или 1 - одному клиенту ?\n2 - чтобы выйти из приложения. ");
+                    int s = Convert.ToInt32(Console.ReadLine());
+                    switch (s)
+                    {
+                        case 1:
+                            {
+                                Console.WriteLine("Введите ID клиента:");
+                                ID_ToClient = Convert.ToInt32(Console.ReadLine());
+
+                                Console.WriteLine("Введите сообщение: ");
+                                textMsg = Console.ReadLine().ToString();
+
+                                MyChannel(endPoint, ref msg, ref h_msg, ref client, Messages.M_TEXT, ID_ToClient, myId, textMsg);
+                                if (h_msg.m_Type == Messages.M_CONFIRM) Console.WriteLine("Сообщение отправлено!");
+                                else Console.WriteLine("Сообщение не было отправлено!");
+
+                                break;
+                            }
+                        case 0:
+                            {
+                                Console.WriteLine("Введите сообщение: ");
+                                textMsg = Console.ReadLine().ToString();
+                                MyChannel(endPoint, ref msg, ref h_msg, ref client, Messages.M_TEXT, (int)Members.M_ALL , myId, textMsg);
+
+                                if (h_msg.m_Type == Messages.M_CONFIRM) Console.WriteLine("Сообщение отправлено!");
+                                else Console.WriteLine("Сообщение не было отправлено!");
+
+                                break;
+                            }
+                        case 2:
+                            {
+                                MyChannel(endPoint, ref msg, ref h_msg, ref client, Messages.M_EXIT, 0, myId);
+                                if (h_msg.m_Type == Messages.M_CONFIRM) Console.WriteLine("Вы успешно вышли!");
+                                else Console.WriteLine("Ошибка.");
+                                Environment.Exit(0);
+                                return;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("Ошибка ввода. Выход!");
+                                Environment.Exit(0);
+                                return;
+                            }
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода. Выход!");
+                return;
+            }
+        }
+    }
+}
